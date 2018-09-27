05a45fc9736a67852879b749f80dc82b987e9cdd
eddy.de.boer <eddy.de.boer@medicore.nl>
2018-09-27 16:24:12 +0200

    Last execercise left

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,52 @@
 (ns p-p-p-pokerface)
 
+(def replacements {\A 14, \K 13, \Q 12, \J 11, \T 10})
+
 (defn rank [card]
-  nil)
+  (let [[rnk _] card]
+    (if (Character/isDigit rnk)
+      (Integer/valueOf (str rnk))
+      (Integer/valueOf (replacements rnk)))))
 
 (defn suit [card]
-  nil)
+  (let [[_ snd] card] (str snd)))
+
+(defn n-of-a-kind? [hand f n]
+  (= n (apply f (vals (frequencies (map rank hand))))))
 
 (defn pair? [hand]
-  nil)
+  (n-of-a-kind? hand max 2))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind? hand max 3))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (n-of-a-kind? hand max 4))
 
 (defn flush? [hand]
-  nil)
+  (= 5 (apply max (vals (frequencies (map suit hand)))) (apply min (vals (frequencies (map suit hand))))))
 
 (defn full-house? [hand]
-  nil)
+  (and (n-of-a-kind? hand max 3) (n-of-a-kind? hand min 2)))
 
 (defn two-pairs? [hand]
-  nil)
+  (= [1 2 2] (sort (vals (frequencies (map rank hand))))))
+
+(defn in? [seq elem]
+  (boolean (some #(= % elem) seq)))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        sorted (sort ranks)
+        sorted-with-low-ace (sort (replace {14 1} sorted))
+        min-elem (apply min sorted)
+        min-elem-with-low-ace (apply min sorted-with-low-ace)
+        expected (range min-elem (+ min-elem 5))
+        expected2 (range min-elem-with-low-ace (+ min-elem-with-low-ace 5))]
+    (or (= sorted expected)
+        (= sorted-with-low-ace expected2))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (straight? hand) (flush? hand)))
 
-(defn value [hand]
-  nil)
+(defn value [hand] nil)
